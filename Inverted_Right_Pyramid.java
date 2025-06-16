7df97741e0297b52ac39b9aa165df9cd7ae7a6e5
JayeshPrabhu <jayeshvasantprabhu@gmail.com>
2025-06-17 00:12:20 +0530

    Input Format: N = 6 Result: * * * * * * * * * * * * * * * * * * * * *

---
+++ Inverted_Right_Pyramid.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Inverted_Right_Pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number of rows");
+        int num = sc.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            for (int j = num; j >i; j--) {
+                System.out.print("*");
+            } System.out.println(" ");
+        }
+    }
+}
